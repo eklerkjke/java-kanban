34fb24a176564e87898a3c7366ac4885c8d4c467
vladimir <klepikov@mdcm.co>
2024-03-20 15:16:45 +0500

    Спринт 8. Четвертая версия

--- src/history/InMemoryTaskHistoryManager.java
+++ src/history/InMemoryTaskHistoryManager.java
@@ -16,7 +16,7 @@ public class InMemoryTaskHistoryManager implements TaskHistoryManager {
     /**
      * Свойство для хранения задачп
      */
-    private final Map<Integer, Node<Task>> historyMap = new HashMap<>();
+    protected final Map<Integer, Node<Task>> historyMap = new HashMap<>();
 
     /**
      * Свойство "головы" истории задач
